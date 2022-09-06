e74f1acd4f1dd80ec8b4660d1551d3d7bdcfd2ba
Nirav Shah <nshah28@ncsu.edu>
2022-09-05 20:47:32 -0400

    Adding test case for Num

---
+++ Code/test_num.py
@@ -0,0 +1,18 @@
+import pytest
+from Num import Num
+
+class TestNum:
+
+    def test_num(self):
+        numDict = {"Zom":[0,1,2,3,4,5,6,7,8,9]}
+        for col, data in numDict.items():
+            if col[0]:
+                num = Num(0,col)
+                for val in data:
+                    num.add(val)
+
+
+                number = num.nums()
+                median = num.mid()
+                sd = num.div()
+                assert number == [0,1,2,3,4,5,6,7,8,9] and median == 5 and 3.1 <= sd and sd <= 4.1
