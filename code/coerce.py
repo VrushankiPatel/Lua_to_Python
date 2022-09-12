1b8d0a59b3b53a108464e30e61eb5720f1c338d3
hellgirl16 <vgandhi@ncsu.edu>
2022-09-12 11:32:56 -0400

    added the cli function for the and test case

--- code/coerce.py
+++ code/coerce.py
@@ -1,17 +1,42 @@
 import re
 
+class the:
+	def __init__(self) -> None:
+		self.the = {}
+		self.help = """CSV : summarized csv file
+        (c) 2022 Tim Menzies <timm@ieee.org> BSD-2 license
+        USAGE: lua seen.lua [OPTIONS]
+        OPTIONS:
+        -e  --eg        start-up example                      = nothing
+        -d  --dump      on test failure, exit with stack dump = false
+        -f  --file      file with csv data                    = ../data/auto93.csv
+        -h  --help      show help                             = false
+        -n  --nums      number of nums to keep                = 512
+        -s  --seed      random number seed                    = 10019
+        -S  --seperator feild seperator                       = ,"""
 
-def coerce(s: str):
-	try: 
-		return int(s)
-	except ValueError:
-		try:
-			return float(s)
+	def coerce(self,s: str):
+		try: 
+			return int(s)
 		except ValueError:
-			match = re.search("^%s*(.−)%s*$")
-			if match == "true":
-				return True
-			elif match == "false":
-				return False
-			else:
-				return match
+			try:
+				return float(s)
+			except ValueError:
+				match = re.search("^%s*(.−)%s*$")
+				if match == "true":
+					return True
+				elif match == "false":
+					return False
+				else:
+					return match
+
+
+
+	def func_the(self):
+		help = self.help
+		reexp = r"[-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)"
+		dictre = re.findall(reexp,help)
+		for key , value in dictre:
+				self.the[key] = self.coerce(value)
+		return self.the
+
--- test/test_num.py
+++ test/test_num.py
@@ -1,6 +1,6 @@
 import pytest
 from num import Num
-
+from coerce import the
 
 class TestNum:
     def test_num(self):
@@ -16,3 +16,12 @@ class TestNum:
                 median = num.mid()
                 sd = num.div()
                 assert number == [0,1,2,3,4,5,6,7,8,9] and median == 5 and 3.1 <= sd and sd <= 4.1
+
+
+    def test_bignum(self):
+        num = Num()
+        the['nums'] = 32
+        for i in range(1000):
+            num.add(i+1)
+        assert len(num._has) == 32
+
