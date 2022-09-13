370477584564ca4d51ba3e9b501a332b4e66e7a0
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-09-13 00:34:34 -0400

    Minor refactor and added o, oo

--- code/utils.py
+++ code/utils.py
@@ -1,22 +1,25 @@
 import math
 import re
-from typing import Callable
+from typing import Callable, Any
 
 
-def coerce(self, s):
-        def fun(s1):
-            if s1.lower()=='true':
-                return True
-            if s1.lower()=='false':
-                return False
-            return s1
-        if s.isdigit(): 
-            return int(s)
-        try: 
-            float(s)
-            return float(s)
-        except ValueError: 
-            return fun(re.match("^\s*(.*?)\s*$", s).groups()[0])
+def coerce(s):
+    def fun(s1):
+        if s1.lower() == 'true':
+            return True
+        if s1.lower() == 'false':
+            return False
+        return s1
+
+    if s.isdigit():
+        return int(s)
+
+    try:
+        return float(s)
+
+    except ValueError:
+        return fun(re.match("^\\s*(.*?)\\s*$", s).groups()[0])
+
 
 # def cli(t: dict):
 #     for slot, v in t.items():
@@ -34,8 +37,8 @@ def per(t: dict, p: int):
     return q
 
 
-def push(t: dict, x):
-    t[len(t) + 1] = x
+def push(t: list, x):
+    t.append(x)
 
     return x
 
@@ -54,10 +57,36 @@ def csv(fname: str, fun: Callable):
             fun(t)
 
 
+def o(t: Any):
+    # if not isinstance(t, dict):
+    #     return str(t)
+    #
+    # def show(_k, _v):
+    #     if re.search(str(k), "^_") is not None:
+    #         _v = o(_v)
+    #
+    #         return str.format(":%s %s", _k, _v) if len(t) == 0 else str(_v)
+    #
+    # u = {}
+    #
+    # for k, v in t.items():
+    #     u[1 + len(u)] = show(k,v)
+    #
+    # if  len(t) == 0:
+    #     table.sort(u)
+    #
+    # return "{" + table.concat(u, " ") +  "}"
+
+    return t
+
+def oo(row):
+    print(o(row))
+
+
 class the:
-	def __init__(self) -> None:
-		self.the = {}
-		self.help = """CSV : summarized csv file
+    def __init__(self) -> None:
+        self.the = {}
+        self.help = """CSV : summarized csv file
         (c) 2022 Tim Menzies <timm@ieee.org> BSD-2 license
         USAGE: lua seen.lua [OPTIONS]
         OPTIONS:
@@ -69,11 +98,12 @@ class the:
         -s  --seed      random number seed                    = 10019
         -S  --seperator feild seperator                       = ,"""
 
+    def func_the(self):
+        help = self.help
+        reexp = r"[-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)"
+        dictre = re.findall(reexp, help)
+        for key, value in dictre:
+            self.the[key] = self.coerce(value)
+        return self.the
 
-	def func_the(self):
-		help = self.help
-		reexp = r"[-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)"
-		dictre = re.findall(reexp,help)
-		for key , value in dictre:
-				self.the[key] = self.coerce(value)
-		return self.the
+
