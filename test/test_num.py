78d306c38c4bcbebd023ae2d26b0c7490a0ec2b6
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-09-13 00:38:31 -0400

    Minor refactor

--- test/test_num.py
+++ test/test_num.py
@@ -1,44 +1,29 @@
-import pytest
 from num import Num
 from the import the
-
-def o(d):
-    return str(d).replace(":", "").replace(",", ":").replace("'", "")
-
-def oo(d):
-	print(o(d))
-	pass
-
+from utils import oo
 
 
 class TestNum:
-
     def test_the(self):
         oo(the)
         assert True
 
     def test_num(self):
-        numDict = {"Zom":[0,1,2,3,4,5,6,7,8,9]}
+        numDict = {"Zom": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}
         for col, data in numDict.items():
             if col[0]:
-                num = Num(0,col)
+                num = Num(0, col)
                 for val in data:
                     num.add(val)
-
 
                 number = num.nums()
                 median = num.mid()
                 sd = num.div()
-                assert number == [0,1,2,3,4,5,6,7,8,9] and median == 5 and 3.1 <= sd and sd <= 4.1
+                assert number == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] and median == 5 and 3.1 <= sd <= 4.1
 
     def test_bignum(self):
-        num = Num(0,"")
-        he = {'nums':50}
-        for i in range(1,500):
+        num = Num(0, "")
+        he = {'nums': 50}
+        for i in range(1, 500):
             num.add(i)
         assert len(num._has) == 50
-
-
-               
-
-
