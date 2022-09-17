6600a10dad975286fd298144210652dc300170de
Niravsshaha2 <111940049+Niravsshaha2@users.noreply.github.com>
2022-09-17 00:45:09 -0400

    Update row.py

--- code/row.py
+++ code/row.py
@@ -1,10 +1,15 @@
-import copy
 
-
+def copyd(t):
+    if ~isinstance(t,dict):
+        return t
+    u = {}
+    for k in t:
+        u[k] = copydic(t[k])
+    return u
 
 # ‘Row‘ holds one record
 class row:
-    def __init__(self, t):
+    def __init__(self, t={}):
         self.cells = t,  # one record
-        self.cooked = copy.deepcopy(t)  # used if we discretize data
+        self.cooked = copyd(t)  # used if we discretize data
         self.isEvaled = False  # true if y−values evaluated
