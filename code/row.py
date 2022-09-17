02a5d6accdde0ede6bfda016ed35e0c4eb329034
Nirav Shah <nshah28@ncsu.edu>
2022-09-17 01:33:18 -0400

    Updated HW3 Data class

--- code/row.py
+++ code/row.py
@@ -1,15 +1,15 @@
-
-def copyd(t):
+def copydic(t):
     if ~isinstance(t,dict):
         return t
     u = {}
     for k in t:
-        u[k] = copyd(t[k])
+        u[k] = copydic(t[k])
     return u
 
-# ‘Row‘ holds one record
+    
+    
 class row:
-    def __init__(self, t={}):
-        self.cells = t,  # one record
-        self.cooked = copyd(t)  # used if we discretize data
-        self.isEvaled = False  # true if y−values evaluated
+    def __init__(self,t={}):
+        self.cells = t
+        self.cooked = copydic(t)
+        self.isEvaled = False
